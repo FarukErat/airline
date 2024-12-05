fb0b4015047b105c713311f38649a06e251433ba
farukerat <farukerat1327@gmail.com>
2024-12-05 12:36:42 +0300

    simplify import

--- src/main/java/com/example/airline/repository/AirlineRepository.java
+++ src/main/java/com/example/airline/repository/AirlineRepository.java
@@ -1,6 +1,7 @@
 package com.example.airline.repository;
 
+import com.example.airline.model.Airline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AirlineRepository extends JpaRepository<com.example.airline.model.Airline, Long> {
+public interface AirlineRepository extends JpaRepository<Airline, Long> {
 }
