6fa0c618ba7c15ab05060b333322ab154b6abc56
farukerat <farukerat1327@gmail.com>
2024-12-05 13:48:13 +0300

    implement password hashing

--- src/main/java/com/example/airline/controller/AuthenticationController.java
+++ src/main/java/com/example/airline/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.example.airline.dto.LoginRequest;
 import com.example.airline.dto.RegisterRequest;
 import com.example.airline.model.UserDetails;
 import com.example.airline.service.UserService;
+import com.example.airline.service.PasswordHasher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,9 @@ public class AuthenticationController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PasswordHasher passwordHasher;
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest) {
         Optional<UserDetails> existingUser = userService.getUserByUsername(registerRequest.username);
@@ -29,9 +33,11 @@ public class AuthenticationController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
         }
 
+        String hashedPassword = passwordHasher.hashPassword(registerRequest.password);
+
         UserDetails newUser = new UserDetails();
         newUser.setUsername(registerRequest.username);
-        newUser.setHashedPassword(registerRequest.password);
+        newUser.setHashedPassword(hashedPassword);
 
         UserDetails savedUser = userService.saveUser(newUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
@@ -43,7 +49,8 @@ public class AuthenticationController {
 
         if (existingUser.isPresent()) {
             UserDetails user = existingUser.get();
-            if (user.getHashedPassword().equals(loginRequest.password)) {
+
+            if (passwordHasher.verifyPassword(loginRequest.password, user.getHashedPassword())) {
                 return ResponseEntity.ok("Login successful");
             } else {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid password");
@@ -53,4 +60,3 @@ public class AuthenticationController {
         }
     }
 }
-
