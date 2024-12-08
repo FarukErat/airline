35660583dbf5bb78783a8fdb2f4c1233d3670830
farukerat <farukerat1327@gmail.com>
2024-12-08 13:48:11 +0300

    remove models and repositories

--- src/main/java/com/example/airline/repository/UserRepoistory.java
+++
@@ -1,8 +0,0 @@
-package com.example.airline.repository;
-
-import com.example.airline.model.UserDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepoistory extends JpaRepository<UserDetails, Long> {
-    UserDetails findByUsername(String username);
-}
