1c721d9a6556bbacacf82d6d469ec5a8a452957d
farukerat <farukerat1327@gmail.com>
2024-12-11 01:28:17 +0300

    add script to install a jar file

---
+++ install_jar.sh
@@ -0,0 +1,6 @@
+mvn install:install-file \
+    -Dfile=./libs/flightway-0.0.1-SNAPSHOT.jar \
+    -DgroupId=com.example \
+    -DartifactId=flightway \
+    -Dversion=0.0.1-SNAPSHOT \
+    -Dpackaging=jar
